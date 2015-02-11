e2e44b2f6145798e4083ecd8c2480085c3b6856e
Eric Heinzman <ehu9079@ehu9079-VirtualBox.(none)>
2015-02-10 22:03:30 -0500

    Added sections that deal with if the incoming symbol is left or right parenthesis and what to do with them when moving them onto stack or not

--- postfix.js
+++ postfix.js
@@ -20,5 +20,28 @@
 		if(equation[i] >= '0' && equation[i] <= '9') {
 			postfix = postfix + equation[i];
 	}
+	
+	//second if the stack is empty or has a left parenthesis on
+	//top push the incoming operator onto the stack
+	else if (stack.length() == 0 || stack.peek() == '(') {
+		stack.push(equation[i]);
 	}
+	//third if the incoming symbol is a left parenthesis push it on stakc
+	else if (equation[i] == '(') {
+		stack.push(equaiton[i]);
+	}
+
+	//fourth if the incoming symbol is a right parenthesis, pop
+	//the stack and print operators until left parenthesis
+	//after this get rid of pair of parenthesis
+	else if (equation[i] == ')') {
+		while (stack.peek() != '(') {
+			postfix = postfix + stack.pop();
+		}
+		stack.pop(); //discarding the parethesis
+	}
+	//fifth if the incoming symbol has a higher precedence than the
+	//top of the stack push it on the stack
+
+
 
